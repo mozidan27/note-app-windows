e0514fd45625ef2bf78fc79bf4d6ee6b9deeecb4
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-02-24 13:09:29 +0200

    sheet border

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -12,6 +12,9 @@ class NotesView extends StatelessWidget {
         backgroundColor: Colors.blue,
         onPressed: () {
           showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
             context: context,
             builder: (context) {
               return const AddNoteBottomSheet();
