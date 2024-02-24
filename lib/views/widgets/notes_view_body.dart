5574dd2fc9569c0d1935ca7c0d9ff7837fee64c3
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-02-24 09:55:33 +0200

    note item created

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/views/widgets/custom_appbar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -14,6 +15,48 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: const Color(0xffffcc80)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 28),
+            ),
+            subtitle: Text(
+              'Hello This is Zidan',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.4), fontSize: 18),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+          Text(
+            'Feb 24, 2024',
+            style: TextStyle(color: Colors.black.withOpacity(0.4)),
+          ),
         ],
       ),
     );
