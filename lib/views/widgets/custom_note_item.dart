3e17867604951178331daccd5e3852c0337dc17c
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-02-27 08:27:42 +0200

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,55 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        bottom: 24,
-        top: 24,
-        left: 18,
-      ),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffffcc80)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 28),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          bottom: 24,
+          top: 24,
+          left: 18,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color(0xffffcc80)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 28),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Hello This is Zidan',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 16),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'Hello This is Zidan',
+                'Feb 24, 2024',
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 16),
+                  color: Colors.black.withOpacity(0.5),
+                ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'Feb 24, 2024',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
